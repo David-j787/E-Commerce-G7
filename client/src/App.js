638ab19642465720fc13f1b34beda65819919d29
unknown <francofyzz15@gmail.com>
2022-03-15 19:33:07 -0300

    Feature SearchBar Component created

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import CreateUser from './components/CreateUser';
+import SearchBar from './components/SearchBar';
+
 
 function App() {
   return (
     <div className="App">
       <CreateUser/>
+      <SearchBar/>
     </div>
   );
 }
