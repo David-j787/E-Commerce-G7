67ba049c1512b17c37e768960d96b04f2906f939
unknown <francofyzz15@gmail.com>
2022-03-16 13:19:05 -0300

    Feature Login

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -6,7 +6,7 @@ export default function Login() {
   return (
     <>
     <form> 
-        <input type="text" placeholder="enter your email" id="txtEmail"/>
+        <input type="text" placeholder="Enter your email" id="txtEmail"/>
         <input type="password" placeholder="Enter your password" id="txtPassword"/>
         <input type="button" value='Enter' id='btnEnter'/>
     </form> 
