077687eb044aca47841fe5a6ec2b651dfe863ef1
Braian Serra <bnserra@gmail.com>
2022-03-16 01:45:45 -0300

    Feature(routes/products): Added filter on endpoint /products with name & category url queries

--- api/src/routes/allProducts.js
+++ api/src/routes/allProducts.js
@@ -1,10 +1,11 @@
 const { Router } = require("express");
-const { Product } = require("../db");
 const allProducts = Router();
+const { getProducts } = require('../utils/getProducts');
 
 allProducts.get("/", async (req, res) => {
   try {
-    const products = await Product.findAll({});
+    const { name = '', category = '' } = req.query;
+    const products = await getProducts(name, category);
     res.json(products);
   } catch (err) {
     res.status(404).send(err);
---
+++ api/src/utils/getProducts.js
@@ -0,0 +1,34 @@
+const { Product, Category } = require('../db.js');
+
+module.exports = {
+    getProducts : async (name, category) => {
+    let products = await Product.findAll({include: Category});
+    products = await products.map(product => ({
+        id: product.id,
+        name: product.name,
+        description: product.description,
+        images: product.images,
+        stock: product.stock,
+        categories: product.categories?.map(category => category.name),
+        rating: product.rating
+    }));
+    console.log(products)
+    
+    let toFilter = [...products];
+
+    // Aplico los filtros en caso que existan
+    if(name !== '') toFilter = toFilter.filter(product => product.name.toLowerCase().includes(name.toLowerCase()));
+    if(category !== '') toFilter = toFilter.filter(product => product.categories?.includes(category));
+
+    // Ordeno el arreglo de manera ascendente, en caso de contener ambas fuentes, mixeo los datos y los deja ordenados
+    toFilter.sort((a, b) => {
+        const A = a.name.toLowerCase();
+        const B = b.name.toLowerCase()
+        if (A < B) return -1;
+        if (A > B) return 1;
+        return 0;
+    })
+    
+    return toFilter.length ? toFilter : 'No se encontraron coincidencias en Bases de Datos';
+    }
+}
