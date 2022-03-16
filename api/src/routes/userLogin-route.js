db9c7d5285d01d80633302ede122c5279008b0dc
Braian Serra <bnserra@gmail.com>
2022-03-16 18:48:51 -0300

    Feature(route /login): finished POST /login to validate user credentials for LOGIN

--- api/src/routes/userLogin-route.js
+++ api/src/routes/userLogin-route.js
@@ -5,8 +5,8 @@ const { userLogin } = require('../utils/userLogin-utils');
 login.post("/", async (req, res) => {
   try {
     const { username, password } = req.body;
-    const userLogged = await userLogin(username);
-    res.json(username);
+    const userLogged = await userLogin(username, password);
+    res.json('Users credentials corrects');
   } catch (error) {
     res.status(404).json('Ocurrió un error: '+ error);
   }
