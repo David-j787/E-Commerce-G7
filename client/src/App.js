4336f44c3d1d14dbfbb7546b024d420b736fb5d8
unknown <francofyzz15@gmail.com>
2022-03-17 12:41:48 -0300

    App configured

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,23 @@
 import CreateUser from './components/CreateUser';
 import SearchBar from './components/SearchBar';
 import Login from './components/Login/Login.jsx';
-<<<<<<< HEAD
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
-=======
 import Navbar from './components/Navbar';
 import CreateProduct from './components/CreateProduct';
->>>>>>> mirror
 import './styles/styles.scss';
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <Router>
         <Switch>
           <CreateUser/>
           <SearchBar/>
           <Route path='/Login' component={Login}/>
+          <Navbar />
+          <CreateProduct/>
         </Switch>
       </Router>
-=======
-      {/* <CreateUser /> */}
-      {/* <SearchBar /> */}
-      {/* <Login /> */}
-      <Navbar />
-      <CreateProduct/>
->>>>>>> mirror
     </div>
   );
 }
