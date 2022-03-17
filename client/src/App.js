a73c95e37877f771aaff89f944a6cbf4adb115ed
unknown <francofyzz15@gmail.com>
2022-03-17 10:28:58 -0300

    Feature_Login complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
 import CreateUser from './components/CreateUser';
 import SearchBar from './components/SearchBar';
 import Login from './components/Login/Login.jsx';
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import './styles/styles.scss';
 
 function App() {
   return (
     <div className="App">
-      <CreateUser/>
-      <SearchBar/>
-      <Login/>
+      <Router>
+        <Switch>
+          <CreateUser/>
+          <SearchBar/>
+          <Route path='/Login' component={Login}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
