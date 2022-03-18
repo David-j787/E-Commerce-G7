3f107190090b7901c1251a32e90e961f8bb606bf
Luis Zepeda <thexepe@gmail.com>
2022-03-18 12:19:50 -0600

    feat/client add search functionality

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -2,6 +2,7 @@ import {
   GET_CATEGORIES,
   GET_PRODUCT_DETAIL,
   GET_ALL_PRODUCTS,
+  GET_SEARCH_PRODUCTS,
 } from '../actions';
 
 const initialState = {
@@ -29,6 +30,20 @@ function rootReducer(state = initialState, action) {
         ...state,
         products: action.payload,
       };
+
+    case GET_SEARCH_PRODUCTS:
+      if (action.payload) {
+        return {
+          ...state,
+          products: state.products.filter( p => {
+            return p.name.toLowerCase().includes(action.payload.toLowerCase())
+          })
+        }
+      } else {
+        return {
+          ...state
+        }
+      }
 
     default:
       return state;
