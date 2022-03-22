a6ecc02b6b54d8a0bba295a35b7240a9e4450875
Jordy Tirado Torres <jordytiradotorres20@gmail.com>
2022-03-21 21:28:27 -0500

    feat(styles/paginate): adding styles for pagination buttons

--- client/src/components/Paginate.jsx
+++ client/src/components/Paginate.jsx
@@ -4,9 +4,9 @@ export function Paginate({productsAmount, productsPerPage, paginate, currentPage
     const lastPage = Math.ceil(productsAmount / productsPerPage);
 
     return(
-        <nav >
-            {currentPage !== 1 &&<button onClick={() => paginate(currentPage - 1)} disabled={currentPage === 1}>Prev</button>}
-            {currentPage !== lastPage &&<button onClick={() => paginate(currentPage + 1)} disabled={currentPage === lastPage}>Next</button>}
+        <nav className='paginate'>
+            {currentPage !== 1 &&<button className='paginate__button' onClick={() => paginate(currentPage - 1)} disabled={currentPage === 1}>Prev</button>}
+            {currentPage !== lastPage &&<button className='paginate__button' onClick={() => paginate(currentPage + 1)} disabled={currentPage === lastPage}>Next</button>}
         </nav>
     )
 }
