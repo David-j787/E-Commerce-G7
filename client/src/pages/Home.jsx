686d45fafeadee601f6f5a8d1687be6447c629b2
Braian Serra <bnserra@gmail.com>
2022-03-21 22:58:47 -0300

    Hotfix: Paginate buttoms

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,15 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllProducts } from '../redux/actions';
 import Products from '../components/Products';
-import Paginado from '../components/Paginado';
+import Paginate from '../components/Paginate';
 
 const Home = () => {
   const dispatch = useDispatch();
   const { products } = useSelector((state) => state);
 
-  const [currentPage, setCurrentPage]=React.useState(1);
-  const [productsPerPage,setProductsPerPage] = React.useState(1);
+  const [currentPage, setCurrentPage]= useState(1);
+  const [productsPerPage,setProductsPerPage] = useState(1);
   const indexOfLastProduct = currentPage * productsPerPage;
   const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
   const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);
@@ -27,31 +27,13 @@ const Home = () => {
         <h2>{products}</h2>
       ) : (
         <div>
-           <div>
-
-
-</div>
-        <Products products={currentProducts} />
-        <div className="pagination">
-        <button onClick={() => paginate(currentPage - 1)} disabled={currentPage === 1}>
-          Prev
-        </button>
-        <Paginado
-    productsPerPage={productsPerPage}
-    products={products.length}
-    paginate={paginate}
-/>
-        <button onClick={() => paginate(currentPage + 1)} disabled={currentPage === Math.ceil(products.length / productsPerPage)}>
-          Next
-        </button>
-      </div>
-
-      </div>
+          <Products products={currentProducts} />
+          <div className="pagination">
+            <Paginate productsPerPage={productsPerPage} currentPage={currentPage} productsAmount={products.length} paginate={paginate} />
+          </div>
+        </div>
       )}
       </div>)
 }
-  
-  
-
 
 export default Home;
