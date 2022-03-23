49440ebe1c74c669dc83066eabcdf0746594df71
Luis Zepeda <thexepe@gmail.com>
2022-03-23 11:24:25 -0600

    rework get orders by Id, if no Id, brings all orders on the system

--- api/src/utils/getOrders-utils.js
+++ api/src/utils/getOrders-utils.js
@@ -1,8 +1,12 @@
-const { Order, Product, User } = require('../db.js');
+const { Order, Product, User, product_order } = require('../db.js');
+const productDetailRouter = require('../routes/productDetail-route.js');
 
 module.exports = {
-    getOrders : async userid => {
-        let findedOrders = await Order.findAll({where: {userId: userid},include: Product});
+    getOrders : async (userid) => {
+        let findedOrders = await Order.findAll({include: Product, Order, product_order});
+        if (userid !== '') findedOrders = findedOrders.filter( p => {
+            return p.userId === parseInt(userid);
+        })
         if(!findedOrders) throw Error('El usuario no tiene ordenes.');
         return findedOrders;
     }
