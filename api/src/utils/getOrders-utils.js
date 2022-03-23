f9b996174beb299e0d650d2dee2b0873993bf55e
Luis Zepeda <thexepe@gmail.com>
2022-03-23 11:35:12 -0600

    comentarios

--- api/src/utils/getOrders-utils.js
+++ api/src/utils/getOrders-utils.js
@@ -4,9 +4,13 @@ const productDetailRouter = require('../routes/productDetail-route.js');
 module.exports = {
     getOrders : async (userid) => {
         let findedOrders = await Order.findAll({include: Product, Order, product_order});
+
+        // filtramos por el parametro userid si nos lo pasaron
         if (userid !== '') findedOrders = findedOrders.filter( p => {
             return p.userId === parseInt(userid);
         })
+
+        // retornamos lo que se haya filtrado o error
         if(!findedOrders) throw Error('El usuario no tiene ordenes.');
         return findedOrders;
     }
