a5b86df506078a9df13efad9cd3877b8b2362a28
Franco Padularrosa <86844005+Fyzz02@users.noreply.github.com>
2022-03-23 16:46:50 -0300

    Update getOrders-utils.js

--- api/src/utils/getOrders-utils.js
+++ api/src/utils/getOrders-utils.js
@@ -6,8 +6,8 @@ module.exports = {
         let findedOrders = await Order.findAll({include: Product, Order, product_order});
 
         // filtramos por el parametro userid si nos lo pasaron
-        if (userid !== '') findedOrders = findedOrders.filter( p => {
-            return p.userId === parseInt(userid);
+        if (userid !== '') findedOrders = findedOrders.filter( o => {
+            return o.userId === parseInt(userid);
         })
 
         // retornamos lo que se haya filtrado o error
