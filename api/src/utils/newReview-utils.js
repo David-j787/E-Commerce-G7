9c05eeaaa4d44b40ce602bedd54a96ba4895bb07
Luis Zepeda <thexepe@gmail.com>
2022-03-24 11:22:05 -0600

    feat api newReview-utils updates products average as reviews are added

--- api/src/utils/newReview-utils.js
+++ api/src/utils/newReview-utils.js
@@ -1,4 +1,5 @@
-const { Review, User, Product } = require("../db");
+const { fn,col } = require("sequelize");
+const { Review, User, Product, conn } = require("../db.js");
 
 module.exports = {
     newReview: async (review, rate, userId, productId) => {
@@ -9,6 +10,29 @@ module.exports = {
         if (!saveReview) throw Error ('The review cant be saved');
         await saveReview.setUser(userId);
         await saveReview.setProduct(productId);
+
+        // Actualizamos el rating promedio cada vez que se hace un review de un producto
+
+        const productRatings = await Review.findAll({
+            where: { productId: productId },
+            include: Product,
+        })
+
+        const productRatingSum = productRatings.reduce( (sum, review) => {
+            return review.rate + sum
+        },0)
+        
+        const productAverageRating = productRatingSum / productRatings.length
+
+        let product = await Product.findByPk(productId)
+        product.set({
+            rating: productAverageRating
+        })
+
+        product = await product.save()
+
+        console.log(product.rating)
+
         return saveReview;
     }
 }
