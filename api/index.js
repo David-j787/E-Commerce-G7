417bfc668aa38b35bf01058ae10964380fc40f6e
David-j787 <davidpatejo@gmail.com>
2022-03-24 15:37:43 -0300

    delete yarn

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const { conn } = require("./src/db.js");
 require("dotenv").config();
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(process.env.PORT, () => {
     console.log("API listening at 3001"); // eslint-disable-line no-console
   });
