d099da3b3fdd0b48974286ca04bec48799fd8eed
Braian Serra <bnserra@gmail.com>
2022-03-27 14:53:14 -0300

    Fix: Link to pay order on MercadoPago

--- client/src/components/Payments.jsx
+++ client/src/components/Payments.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { Link } from 'react-router-dom';
 
 export default function Payments({ url, clearCart }){
     return(
         <div>
-        <Link className="payments__btn" to={{pathname: url}}><button onClick={clearCart} disabled={!url.length}>Pay order</button></Link>
+        <a className="payments__btn" href={url}><button onClick={clearCart} disabled={!url.length}>Pay order</button></a>
         </div>
     )
 }
