9be3d625e4d3319f039fc3995a04774a454072d1
Jordy Tirado Torres <jordytiradotorres20@gmail.com>
2022-03-27 21:10:33 -0500

    feat(styles/*): add styles for dashboard user

--- client/src/components/UserAccount.jsx
+++ client/src/components/UserAccount.jsx
@@ -24,55 +24,57 @@ export function UserAccount(){
     const [userData, setUserData] = useState({})
 
     return(
-        <div>
-            <div>
-                <h3>Account data</h3>
-            </div>
-            <div>
-                <span>Username: </span> 
-                <span name="user" value={userData.user}>{userData.user}</span>
-            </div>
-            <div>
-                <span>E-mail: </span>
-                <span name="email" value={userData.email}>{userData.email}</span>
-            </div>
+        <div className="userAccount">
+            <div className="container">
+                <div className="userAccount__title">
+                    <h3>Account data</h3>
+                </div>
+                <div>
+                    <span>Username: </span> 
+                    <span name="user" value={userData.user}>{userData.user}</span>
+                </div>
+                <div>
+                    <span>E-mail: </span>
+                    <span name="email" value={userData.email}>{userData.email}</span>
+                </div>
 
-            <div>
-                <h3>Personal Information</h3>
+                <div>
+                    <h3 className="userAccount__title">Personal Information</h3>
+                </div>
+                <div>
+                    <span>Full name: </span> 
+                    <span name="name" value={userData.name}>{userData.name} </span>
+                    <span name="last_name" value={userData.last_name}> {userData.last_name}</span>
+                </div>
+                
+                <div>
+                    <span>Country: </span>
+                    <span name="country" value={userData.country}>{userData.country}</span>
+                </div>
+                
+                <div>
+                    <span>City: </span>
+                    <span name="city" value={userData.city}>{userData.city}</span>
+                </div>
+                
+                <div>
+                    <span>Zip Code: </span>  
+                    <span name="zip_code" value={userData.zip_code}>{userData.zip_code}</span>
+                </div>
+                
+                <div>
+                    <span>Address: </span>
+                    <span name="address" value={userData.address}>{userData.address}</span>
+                </div>
+                
+                <div>
+                    <span>Birthday: </span>
+                    <span name="birthday" value={userData.birthday}>{userData.birthday}</span>
+                </div>
+                
+                    <Link to='/user/account/edit' className="userAccount__button">EDIT ACCOUNT</Link>
+              
             </div>
-            <div>
-                <span>Full name: </span> 
-                <span name="name" value={userData.name}>{userData.name} </span>
-                <span name="last_name" value={userData.last_name}> {userData.last_name}</span>
-            </div>
-            
-            <div>
-                <span>Country: </span>
-                <span name="country" value={userData.country}>{userData.country}</span>
-            </div>
-            
-            <div>
-                <span>City: </span>
-                <span name="city" value={userData.city}>{userData.city}</span>
-            </div>
-            
-            <div>
-                <span>Zip Code: </span>  
-                <span name="zip_code" value={userData.zip_code}>{userData.zip_code}</span>
-            </div>
-            
-            <div>
-                <span>Address: </span>
-                <span name="address" value={userData.address}>{userData.address}</span>
-            </div>
-            
-            <div>
-                <span>Birthday: </span>
-                <span name="birthday" value={userData.birthday}>{userData.birthday}</span>
-            </div>
-            
-            <br></br>
-            <Link to='/user/account/edit'><button>EDIT ACCOUNT</button></Link>
         </div>
     )
 }
