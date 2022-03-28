d9a228ad51cd55f30d1b886f254c375e15aa26fe
Braian Serra <bnserra@gmail.com>
2022-03-28 12:34:54 -0300

    Solving google Auth crash

--- api/src/utils/verifyGoogleToken.js
+++ api/src/utils/verifyGoogleToken.js
@@ -2,16 +2,23 @@ const { OAuth2Client } = require('google-auth-library');
 const client = new OAuth2Client(process.env.CLIENT_ID);
 
 const verifyGoogleToken = async (req, res, next) => {
-    const idToken = req.body.id_token;
-    const response = await client.verifyIdToken({idToken, audience: process.env.CLIENT_ID});
-    const payload = response.getPayload();
-    const userId = payload['sub'];
-    if(payload.sub) {
-        req.body.email = payload.email
-        req.body.name = payload.given_name
-        req.body.last_name = payload.family_name
+    try {
+        const idToken = req.body.id_token;
+        const response = await client.verifyIdToken({idToken, audience: process.env.CLIENT_ID});
+        if(!response) throw Error ('Google Login Auth is not responding')
+        const payload = response.getPayload();
+        const userId = payload['sub'];
+        if(payload.sub) {
+            req.body.email = payload.email
+            req.body.name = payload.given_name
+            req.body.last_name = payload.family_name
+            next();
+        }else return res.status(403);    
+    } catch (error) {
+        res.status(403).json('Error ocurred: ' + error)
         next();
-    }else return res.status(403);    
+    }
+
 }
 
 module.exports = verifyGoogleToken
