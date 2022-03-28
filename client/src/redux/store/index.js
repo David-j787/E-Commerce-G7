3c6556dc4880c244b75a95813fa246aa4caa05d8
Braian Serra <bnserra@gmail.com>
2022-03-28 02:05:16 -0300

    Passed to production

--- client/src/redux/store/index.js
+++ client/src/redux/store/index.js
@@ -1,13 +1,10 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "../reducers/index.js"
 
 export const store = createStore(
   rootReducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  applyMiddleware(thunk)
 );
 
 export default store;
