e745184efb4e6a8adf0a6097ee9e755d7d713fec
Braian Serra <bnserra@gmail.com>
2022-03-28 03:45:40 -0300

    Allow Set SA Permisions

--- client/src/admincp/AdminSideBar.jsx
+++ client/src/admincp/AdminSideBar.jsx
@@ -24,7 +24,7 @@ export default function AdminSideBar({showComponent}){
                 <i className="fa fa-folder-open fa-2x"></i>
                 <span className="nav-text">Orders</span>               
             </li>
-            {user?.roleId === 1 && 
+            {user?.roleId <= 1 && 
             <li onClick={e => showComponent('editRole')}>
                 <i className="fa fa-bar-chart-o fa-2x"></i>
                 <span className="nav-text">Edit roles</span>
