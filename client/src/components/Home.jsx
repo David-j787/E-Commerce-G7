382dfae8f0aed7ab1c468d7ee7c75416c447bab1
mbenetti2010 <mbenetti2010@gmail.com>
2022-04-02 23:47:46 -0300

    feature_cli_chatbot_botones_2_de_3

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -6,9 +6,12 @@ import FrecuentlyVisited from './FrecuentlyVisited';
 import WishlistView from './WishlistView';
 import useUser from './Login/hooks/useUser';
 import WhatsApp from './WhatsApp';
+<<<<<<< HEAD
 import ChatBot from "../components/Chatbot/ChatBot";
 import 'react-chatbot-kit/build/main.css';
 
+=======
+>>>>>>> development
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -24,12 +27,16 @@ const Home = () => {
   return (
     <div className="shop">
       <Slider />
+<<<<<<< HEAD
       <ChatBot />
+=======
+>>>>>>> development
       {!isLogged ? false : 
         <div>
           <FrecuentlyVisited />
           {wishlist?.length ? <WishlistView /> : false}
           <WhatsApp />
+<<<<<<< HEAD
           <ChatBot />
         </div>}
       </div>)
@@ -37,5 +44,10 @@ const Home = () => {
 
   
 ;
+=======
+        </div>}
+      </div>)
+};
+>>>>>>> development
 
 export default Home;
